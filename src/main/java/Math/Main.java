3d86a75cdc59bdddec710f42b066a02f6bf4ffc3
tjip1234 <rafaeldiederen@gmail.com>
2022-05-10 12:52:16 +0200

    commit

--- src/main/java/Math/Main.java
+++ src/main/java/Math/Main.java
@@ -9,35 +9,10 @@ public class Main {
     public static double mass = 0.056;
     public static double friction = 0.08;
     public static void main(String[] args) throws FileNotFoundException {
-        
-        
-        //Math.PhysicsEngine.EulersMethod(0.01, 0, 0, 2, 0, 0.056, 0.05);
-        //Math.PhysicsEngine.SemiImplicitEulerMethod(0.01, 0, 0, 2, 0, 0.056, 0.05);
-        //PrintWriter writer = new PrintWriter("exp.csv");
-        //Math.PhysicsEngine.rungeKutta4(0, 0, 2, 0);
-        //p.EulersMethod(stepsize, 0, 0, 2, 0, 0.056, 0.05);
-        // one more 0 after i += for 100k
-        
-        //l.rungeKutta2(0.000000001, 0, 0, 2, 0);
-
-        
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            
-            e.printStackTrace();
-        }
-        PhysicsEngine d = new PhysicsEngine(0.001);
-        System.out.println("Value, " + "Stepsize, " + "Odesolver");
-        long time = System.currentTimeMillis();
-        //System.out.println(d.rungeKutta4(d.x0, d.y0, 3.3768969127158854, 1.3135526464700689));
-        //double i = 0.0177827941; i< 0.10000000; i+=0.00000822172059
-        //System.out.println(d.NonOscilaiting(d.x0, d.y0,3.3768969127158854, 1.3135526464700689) );
-        
-        
-        
-
-
+        State state = new State(0,0,4,1);
+        OdeSolver solver = new Euler(state,0.001);
+        PhysicsEngine h = new PhysicsEngine(0.001);
+        System.out.println(h.run(solver,state));
     }  
     
 }
--- src/main/java/Math/mathFunction.java
+++ src/main/java/Math/mathFunction.java
@@ -4,6 +4,6 @@ public class mathFunction {
 public static double Function(double x, double y){
  //return 0.05*(x*x + y*y);
  //return 0.1*x+1.0;
- return 0.4d*(0.9d - Math.exp( - (x * x + y * y) / 8.0d) );
+ return 0.5*(Math.sin((x-y)/7)+0.9);
 }
 }
